e9c6db7023868860b2d29b15a1638ed29240634f
Suraj Kumar <surajkumar620103@gmail.com>
2025-10-13 23:00:28 +0530

    feat: add semver internal modules and functionality
    - Introduced internal constants for semver specifications including max lengths and release types.
    - Implemented debugging utilities for semver operations.
    - Developed identifier comparison functions for semver components.
    - Created an LRU cache for efficient version management.
    - Added parsing options for semver inputs.
    - Built comprehensive regex patterns for semver validation and parsing.
    - Updated package.json for semver version 7.7.3 with necessary scripts and dependencies.
    - Implemented preload and range.bnf files for semver functionality.
    - Added range functions for greater than, less than, and intersection checks.
    - Developed min and max satisfying functions for version ranges.
    - Created subset and simplify functions for range management.
    - Added utility functions for converting ranges to comparators and validating ranges.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,8 @@
 const userModel = require("../models/user.models.js")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const secure = "5fdc4caaf31632871dc0318ee80f3ad2"
 
 async function registerUser(req, res) {
 
@@ -11,5 +15,41 @@ async function registerUser(req, res) {
         })
     }
 
-    
+    const hashPassword = await bcrypt.hash(password, 10)
+    console.log(hashPassword);
+
+    const user = await userModel.create({
+        fullname,
+        email,
+        password: hashPassword
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+    }, secure)
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "strict",     // protects against CSRF attacks
+        maxAge: 7 * 24 * 60 * 60 * 1000 // cookie expires in 7 days
+    })
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullname: user.fullname
+        }
+    })
 }
+
+async function loginUser(req, res) {
+
+}
+
+async function logoutUser(req, res) {
+
+}
+
+module.exports = { registerUser, loginUser, logoutUser }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const autController = require("../controllers/auth.controller.js")
 
 const router = express.Router()
 
 
-router.post("/user/register")
+router.post("/user/register", autController.registerUser)
+router.post("/user/login", autController.loginUser)
+router.post("/user/logout", autController.logoutUser)
 
 
 module.exports = router
