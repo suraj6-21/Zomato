362b95afb6d0b281aa71913a1c54b22c06ad85ba
Suraj Kumar <surajkumar620103@gmail.com>
2025-11-08 15:00:27 +0530

    update

--- backend/src/services/storage.services.js
+++ backend/src/services/storage.services.js
@@ -3,7 +3,6 @@ const multer = require("multer");
 const ImageKit = require("imagekit")
 
 
-
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
